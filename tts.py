c425a085f690f903669d12479fe97f5a36dfc155
jingyao-d <129810762+jingyao-d@users.noreply.github.com>
2025-05-03 14:21:38 -0700

    Add files via upload
    text to speech, not done as a subprocess

---
+++ tts.py
@@ -0,0 +1,28 @@
+from gtts import gTTS
+import pygame
+import os
+import time
+
+language = 'en'
+
+pygame.mixer.init()
+
+def say(self, text = "default response"):
+    myobj = gTTS(text=text, lang=language, slow=False)
+
+    myobj.save("speech.mp3")
+
+    try: 
+        pygame.mixer.music.load("speech.mp3")
+        pygame.mixer.Channel(0).play(pygame.mixer.Sound('speech.mp3'))
+        while pygame.mixer.Channel(0).get_busy():
+            time.sleep(.1)
+    finally:
+        pygame.mixer.music.stop()
+        if os.path.exists():
+            os.remove("speech.mp3")
+
+# example code
+#
+# tts = textToSpeech()
+# tts.say("hello")
